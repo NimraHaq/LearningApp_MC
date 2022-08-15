c8c536eee4d05c7c78a9b941db4526ad67c9f039
Nimra Haq <nimrahaq10@gmail.com>
2022-08-15 23:08:24 +0500

    Created adaptor class

---
+++ app/src/main/java/com/example/learningapp/MyAdaptor.java
@@ -0,0 +1,41 @@
+package com.example.learningapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class MyAdaptor extends ArrayAdapter
+{
+    String[] ListText;
+    Integer[] Images;
+    Activity context;
+
+    public MyAdaptor(Activity context, String[] listT, Integer[] img) {
+        super(context, R.layout.row, R.id.listViewImage, img);
+        this.ListText = listT;
+        this.Images = img;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflator = context.getLayoutInflater();
+        View view = inflator.inflate(R.layout.row, parent, false);
+        TextView textView =  view.findViewById(R.id.listViewText);
+        ImageView imageView =  view.findViewById(R.id.listViewImage);
+
+        textView.setText(ListText[position]);
+        imageView.setImageResource(Images[position]);
+
+        return view;
+    }
+}
